360903fdb0e0de6e017f81ffa3b19a967ced00e4
Thijs Janssen <thijs@seecr.nl>
2021-02-02 14:52:47 +0100

    TJ: license.

--- test/alltests.sh
+++ test/alltests.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 ## begin license ##
 #
-# "Seecr Test Extra" provides extra test tools - which bring in lots of dependencies.
+# "Meresco Testsupport" provides extra test tools.
 #
-# Copyright (C) 2019 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2019, 2021 Seecr (Seek You Too B.V.) https://seecr.nl
 #
-# This file is part of "Seecr Test Extra"
+# This file is part of "Meresco Testsuport"
 #
-# "Seecr Test Extra" is free software; you can redistribute it and/or modify
+# "Meresco Testsuport" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "Seecr Test Extra" is distributed in the hope that it will be useful,
+# "Meresco Testsuport" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Seecr Test Extra"; if not, write to the Free Software
+# along with "Meresco Testsuport"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
